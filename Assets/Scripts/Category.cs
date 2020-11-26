0696fbcb4a59a8fb8a2d6f02b9ff6c1606e2ebe6
hawkcrash <zoroark40@gmail.com>
2020-11-26 12:02:49 +0100

    Crowd system added

---
+++ Assets/Scripts/Category.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+[Serializable]
+public class Category
+{
+    public int want;
+    public bool LowFrequency;
+    public bool HighFrequency;
+    public bool Melodic;
+    public bool Rhythmic;
+}
---
+++ Assets/Scripts/Crowd.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Crowd")]
+public class Crowd : ScriptableObject
+{
+    public Category chill;
+
+    public Category Aggressive;
+
+    public Category Energic;
+
+    public Category Calm;
+
+    public Skybox Skybox;
+
+    public int Top;
+
+    public int Mid;
+
+    public int Bot;
+
+
+}
