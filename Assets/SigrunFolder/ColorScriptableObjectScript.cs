33c8bc16da8ff26f0d768399ba6b924935f9fe70
TheHresvelgian <sigrunhog@hotmail.com>
2020-11-27 11:11:12 +0100

    .

---
+++ Assets/SigrunFolder/ColorScriptableObjectScript.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu]
+public class ColorScriptableObjectScript : ScriptableObject
+{
+    public Color aggressive;
+    public Color energetic;
+    public Color calm;
+    public Color chill;
+}
---
+++ Assets/SigrunFolder/TestTheColor.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestTheColor : MonoBehaviour
+{
+
+    public int musicCategory;
+    public ColorScriptableObjectScript colors;
+
+
+
+    private void Start()
+    {
+        var renderer = GetComponent<Renderer>();
+
+        switch (musicCategory)
+        {
+            case 0:
+                renderer.material.color = new Color(colors.aggressive.r,colors.aggressive.g,colors.aggressive.b);
+                break;
+            case 1:
+                renderer.material.color = new Color(colors.energetic.r, colors.energetic.g, colors.energetic.b);
+                break;
+            case 2:
+                renderer.material.color = new Color(colors.calm.r, colors.calm.g, colors.calm.b);
+                break;
+            case 3:
+                renderer.material.color = new Color(colors.chill.r, colors.chill.g, colors.chill.b);
+                break;
+        }
+    }
+}
