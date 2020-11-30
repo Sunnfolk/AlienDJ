158749b2cfe53af765132df1cdbdfdcfd998939f
TricksterNO <marlped1605@gmail.com>
2020-11-30 14:38:10 +0100

    Lagt til tsjorte kannon mk1

---
+++ Assets/Scripts/TshirtCannon/TshirtProjectile_Script.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TshirtProjectile_Script : MonoBehaviour
+{
+    public float speed = 40f; //Speed of the projectile
+
+    private Rigidbody _rb; //projectile rigidbody
+
+    private void Start() 
+    {
+        Rigidbody _rb = GetComponent<Rigidbody>(); //getting the rigidbody of the projectile
+        _rb.velocity = transform.forward * speed;
+    }
+    private void Update() 
+    {
+        transform.Rotate (Random.Range(-90f, 90f)*Time.deltaTime*speed, Random.Range(-90f, 90f)*Time.deltaTime*speed,Random.Range(-80f, 80f)*Time.deltaTime*speed);    
+    }
+    private void OnTriggerEnter(Collider other) 
+    {
+        if (other.CompareTag("crowd"))
+        {
+            Destroy(this.gameObject);
+        }
+        if (other.CompareTag("wall"))
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+
+}
