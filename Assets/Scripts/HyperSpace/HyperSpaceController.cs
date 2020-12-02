2c58e4ff3530b92594d2aaf8b09c665888b98051
GameDevTeacher <markus@gamedevteacher.com>
2020-12-02 19:26:14 +0100

    Initial Commit
    Added generic Hyperspace functionality with a moving tunnel and a Hyperspace controller with comments set up for functionality.

---
+++ Assets/Scripts/HyperSpace/HyperSpaceController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace HyperSpace
+{
+    public class HyperSpaceController : MonoBehaviour
+    {
+        public GameObject tunnel;
+
+        public void RunHyperSpace()
+        {
+            StartCoroutine(nameof(InitialiseHyperSpace));
+        }
+
+        private IEnumerator InitialiseHyperSpace()
+        {
+            // Light Flash (HyperSpace Enter)
+            // Hyperspace start audio
+            // Rotate Menu
+            
+            yield return new WaitForSeconds(1f);
+            
+            // Hyperspace tunnel loop audio
+            tunnel.SetActive(true);
+            // Set new skybox, crowd, planet ambience and so on
+            
+            yield return new WaitForSeconds(3f);
+            
+            // Hyperspace end audio
+            // light flash (HyperSpace Exit)
+            
+            yield return new WaitForSeconds(1f);
+            
+            // Start Crowd Spawn
+            // StopCoroutine
+        }
+        
+    }
+}
