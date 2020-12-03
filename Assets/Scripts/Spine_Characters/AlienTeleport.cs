e9433da3fe17b1d81bb75498ec4cf2781ff1a1da
GameDevTeacher <markus@gamedevteacher.com>
2020-12-03 10:23:48 +0100

    Refactored Alien Teleport Script
    Made it more readable
    Added Uranus character - must be fixed due to a centering issue

---
+++ Assets/Scripts/Spine_Characters/AlienTeleport.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Spine.Unity;
+using UnityEngine;
+using UnityEngine.VFX;
+using Random = System.Random;
+
+public class AlienTeleport : MonoBehaviour
+{
+    [Header("VFX")]
+    public VisualEffect effect;
+    [Space(5f)]
+    [Header("Teleport Variables")]
+    [SerializeField] private float _teleportTime;
+    [SerializeField] private float _vfxWaitTime;
+    [SerializeField] private float _spawnRandomMin;
+    [SerializeField] private float _spawnRandomMax;
+    
+    [Space(5f)]
+    [Header("Components")]
+    [SerializeField] private GameObject _SpineRenderer;
+    [SerializeField] private SpriteRenderer _spriteRenderer;
+    
+    
+    private Material _material;
+
+    private float _teleportValue;
+    public float _teleportValueMax;
+
+    private bool _canTeleportIn;
+    private bool _canTeleportOut;
+    
+    private bool _alienCanDespawn;
+
+    private void OnEnable()
+    {
+        _alienCanDespawn = true;
+        _material = _spriteRenderer.material;
+        
+        effect.Stop();
+        _spriteRenderer.enabled = true;
+        _SpineRenderer.SetActive(false);
+        _teleportValue = 0f;
+        
+        StartCoroutine(nameof(TeleportIn), _vfxWaitTime);
+    }
+    
+
+    // Update is called once per frame
+    private void Update()
+    {
+        _teleportValue = Mathf.Clamp(_teleportValue, 0, _teleportValueMax);
+        _material.SetFloat("Vector1_FA25B07E", _teleportValue);
+
+        if (AlienDespawn.StaticTimerisFinishedBool && _alienCanDespawn)
+        {
+            _SpineRenderer.GetComponent<SkeletonAnimation>().AnimationState.SetEmptyAnimation(0, 0.25f);
+            StartCoroutine(nameof(TeleportOut), 1f);
+            _alienCanDespawn = false;
+        }
+        
+        // These if statements cause the teleport value to increase or decrease, thereby visualising the teleport.
+        if (_canTeleportIn)
+        {
+            _teleportValue += _teleportTime;
+        }
+        else if (_canTeleportOut)
+        {
+            _teleportValue -= _teleportTime;
+        }
+        
+        // Check Whether the player should be able to teleport in or out 
+        if (_canTeleportIn && _teleportValue >= _teleportValueMax)
+        {
+            _spriteRenderer.enabled = false;
+            effect.Stop();
+            _SpineRenderer.SetActive(true);
+            _canTeleportIn = false;
+        }
+        else if (_canTeleportOut && _teleportValue <= 0f)
+        {
+            effect.Stop();
+            _canTeleportOut = false;
+        }
+
+    }
+    
+    private IEnumerator TeleportIn(float time)
+    {
+        var timer = UnityEngine.Random.Range(_spawnRandomMin, _spawnRandomMax);
+        yield return new WaitForSeconds(timer);
+        
+        effect.Play();
+        yield return new WaitForSeconds(time);
+        _canTeleportIn = true;
+
+    }
+
+    public IEnumerator TeleportOut(float time)
+    {
+        effect.Play();
+        _spriteRenderer.enabled = true;
+        _SpineRenderer.SetActive(false);
+        yield return new WaitForSeconds(time);
+        _canTeleportOut = true;
+    }
+}
--- Assets/Scripts/Spine_Characters/AnimatedAlienCharacter.cs
+++ Assets/Scripts/Spine_Characters/AnimatedAlienCharacter.cs
@@ -1,43 +1,40 @@
 ﻿using Spine.Unity;
 using UnityEngine;
 
-namespace Spine_Characters
+public class AnimatedAlienCharacter : MonoBehaviour
 {
-    public class AnimatedAlienCharacter : MonoBehaviour
-    {
-        private SkeletonAnimation _skeletonAnimation;
+    private SkeletonAnimation _skeletonAnimation;
 
-        [SerializeField] private AnimationReferenceAsset _boredDance;
-        [SerializeField] private AnimationReferenceAsset _normalDance;
-        [SerializeField] private AnimationReferenceAsset _hypedDance;
+    [SerializeField] private AnimationReferenceAsset _boredDance;
+    [SerializeField] private AnimationReferenceAsset _normalDance;
+    [SerializeField] private AnimationReferenceAsset _hypedDance;
     
         
-        // Start is called before the first frame update
-        private void Start()
+    // Start is called before the first frame update
+    private void Start()
+    {
+        _skeletonAnimation = GetComponent<SkeletonAnimation>();
+        _skeletonAnimation.AnimationState.SetAnimation(0, _boredDance.Animation, true);
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Keypad1))
         {
-            _skeletonAnimation = GetComponent<SkeletonAnimation>();
             _skeletonAnimation.AnimationState.SetAnimation(0, _boredDance.Animation, true);
         }
-
-        // Update is called once per frame
-        private void Update()
+        if (Input.GetKeyDown(KeyCode.Keypad2))
         {
-            if (Input.GetKeyDown(KeyCode.Keypad1))
-            {
-                _skeletonAnimation.AnimationState.SetAnimation(0, _boredDance.Animation, true);
-            }
-            if (Input.GetKeyDown(KeyCode.Keypad2))
-            {
-                _skeletonAnimation.AnimationState.SetAnimation(0, _normalDance.Animation, true);
-            }
-            if (Input.GetKeyDown(KeyCode.Keypad3))
-            {
-                _skeletonAnimation.AnimationState.SetAnimation(0, _hypedDance.Animation, true);
-            }
-            if (Input.GetKeyDown(KeyCode.Keypad0))
-            {
-                _skeletonAnimation.AnimationState.SetEmptyAnimation(0, 0.25f);
-            }
+            _skeletonAnimation.AnimationState.SetAnimation(0, _normalDance.Animation, true);
+        }
+        if (Input.GetKeyDown(KeyCode.Keypad3))
+        {
+            _skeletonAnimation.AnimationState.SetAnimation(0, _hypedDance.Animation, true);
+        }
+        if (Input.GetKeyDown(KeyCode.Keypad0))
+        {
+            _skeletonAnimation.AnimationState.SetEmptyAnimation(0, 0.25f);
         }
     }
 }
