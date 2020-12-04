6d423a6a164526de85b6277bb7451695fc68f98b
TricksterNO <marlped1605@gmail.com>
2020-12-04 13:03:34 +0100

    HighScore

---
+++ Assets/Scripts/DJ Booth/HighScore_Script.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HighScore_Script : MonoBehaviour
+{
+    [SerializeField] private int Score;
+    [SerializeField] private int CurrentScore;
+    [SerializeField] private Score_Script _ScoreScript;
+
+     public bool LevelComplete = false;
+     public List<int> LeaderBoard = new List<int>();
+
+     private void Start() 
+     {
+         
+     }
+
+    private void Update() 
+     {
+         Score = _ScoreScript._Score;
+
+     if (LevelComplete == true)
+     {
+        SaveHighScore();
+     }
+
+     if (Input.GetKeyDown(KeyCode.H))
+     {
+         
+     }
+
+   
+     }
+
+    private void SaveHighScore()
+     {   
+         CurrentScore = Score;
+         LeaderBoard.Add(CurrentScore);
+         LeaderBoard.Sort();
+         LeaderBoard.Reverse();
+         PlayerPrefs.SetInt("ScoreBoard", LeaderBoard.Count);
+         PlayerPrefs.Save();
+     }
+}
