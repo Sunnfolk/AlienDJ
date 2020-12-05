41cdd29f08fb47435177aeee9340de0922cfa7b8
TheHresvelgian <sigrunhog@hotmail.com>
2020-12-05 14:11:51 +0100

    Wrong branch but photosensitivity options xample script

---
+++ Assets/_To Be Reviewed/Sigrun/Photosensitity.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Photosensitity : MonoBehaviour
+{
+    public void EnableHyperspace()
+    {
+        AccessibilityContainer.hyperspaceEnabled = true;
+    }
+
+    public void DisableHyperspace()
+    {
+        AccessibilityContainer.hyperspaceEnabled = false;
+    }
+
+    public void EnableLightshow()
+    {
+        AccessibilityContainer.lightshowsEnabled = true;
+    }
+
+    public void DisableLightshow()
+    {
+        AccessibilityContainer.lightshowsEnabled = false;
+    }
+}
