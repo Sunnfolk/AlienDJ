5631d2f9b7282b755fd734bb12c75a15bf55b18a
GameDevTeacher <markus@gamedevteacher.com>
2020-12-07 10:28:29 +0100

    Finalized Animation

---
+++ Assets/Scripts/Alien Spawn/AlienDespawn.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace Alien_Spawn
+{
+    public class AlienDespawn : MonoBehaviour
+    {
+        public bool currentBool;
+        [SerializeField] public static bool StaticTimerisFinishedBool;
+
+        private void Update()
+        {
+            StaticTimerisFinishedBool = currentBool;
+        }
+    }
+}
