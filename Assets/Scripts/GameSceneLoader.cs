b52089d9d6f039b087140fbf95e28c21d22dedef
GameDevTeacher <markus@gamedevteacher.com>
2020-12-09 10:12:36 +0100

    Added Environment Prefabs and Sceneloader on Start

---
+++ Assets/Scripts/GameSceneLoader.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameSceneLoader : MonoBehaviour
+{
+    public List<SceneAsset> _scenes;
+    // Start is called before the first frame update
+    private void Start()
+    {
+        foreach (SceneAsset scene in _scenes)
+        {
+            SceneManager.LoadScene(nameof(scene), LoadSceneMode.Additive);
+        }
+    }
+}
